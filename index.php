734b5cb2fc3cf9ae22135023a0d307b49a87682c
jtissier78 <jtissier78@gmail.com>
2019-11-26 00:21:15 +0100

    final Commit

--- index.php
+++ index.php
@@ -7,10 +7,11 @@
     <!--[if IE]>
       <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="https://browsehappy.com/">upgrade your browser</a> to improve your experience and security.</p>
     <![endif]-->
-
+    <?php /* include('php/Vfooter.php'); */ ?>
 
     
-  <?php include('php/Vfooter.php'); ?>
+
+    <?php include('php/Vfooter.php'); ?>
     
   </body>
 
